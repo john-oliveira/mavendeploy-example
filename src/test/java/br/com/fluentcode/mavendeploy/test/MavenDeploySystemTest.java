7d55b61f9c5af4bb73dfc08437e2cc09e66db613
elton-oliveira <elton.pmp@gmail.com>
2014-06-20 21:32:17 -0300

    Add selenium test

---
+++ src/test/java/br/com/fluentcode/mavendeploy/test/MavenDeploySystemTest.java
@@ -0,0 +1,36 @@
+package br.com.fluentcode.mavendeploy.test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+
+public class MavenDeploySystemTest {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void begin(){
+		this.driver = new HtmlUnitDriver();
+		this.driver.get("http://localhost:8080/mavendeploy-example");
+	}
+	
+	@After
+	public void end(){
+		this.driver.quit();
+	}
+	
+	@Test
+	public void deveCarregarOIndexJsp(){
+		WebElement h2 = this.driver.findElement(By.tagName("h2"));
+		assertThat(h2.getText(), equalTo("Hello World!"));
+	}
+
+}
